59b83b85793f1d109bbb0801060ef4d48bb97b59
JishanaJ <87926038+JishanaJ@users.noreply.github.com>
2021-08-28 21:38:36 +0530

    Numpy assignment quartions

---
+++ Numpy_Assignement.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Question 1: Use numpy to generate array of 25 random numbers sampled from a standard normal distribution
+
+# In[9]:
+
+
+import numpy as np
+rand_num = np.random.randn(5,5)
+print("25 random numbers from a standard normal distribution:")
+print(rand_num)
+
+
+# In[ ]:
+
+
+
+
